15f030a272b0f1328958a681c1eecbb86bb58ebd
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-04-25 19:39:22 +0200

    Add Books class to index.js through and add select html elements

---
+++ index.js
@@ -0,0 +1,6 @@
+import Books from './modules/books.js';
+
+const bookList = document.querySelector('.bookList');
+const bookTitle = document.querySelector('.book-title');
+const bookAuthor = document.querySelector('.book-author');
+const addBookBtn = document.querySelector('.btn-add');
